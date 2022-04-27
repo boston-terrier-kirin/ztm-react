f55c72643b1909e71ccf83fced0d16a2912ba056
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-28 06:19:35 +0900

    crwn-clothing/Section4-74

---
+++ crwn-clothing/src/App.js
@@ -0,0 +1,38 @@
+const App = () => {
+  return (
+    <dir className="catetories-container">
+      <div className="category-container">
+        <div className="category-body-container">
+          <h2>HATS</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <div className="category-body-container">
+          <h2>JACKETS</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <div className="category-body-container">
+          <h2>SNEAKERS</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <div className="category-body-container">
+          <h2>WOMENS</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <div className="category-body-container">
+          <h2>MENS</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    </dir>
+  );
+};
+
+export default App;
