8a55210902cc642ef58707e2d747c700dc1bf9ef
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-28 09:37:14 +0900

    crwn-clothing/Section4-79

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,42 +1,7 @@
-import './categories.styles.scss';
+import Directory from './components/directory/directory';
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'Hats',
-    },
-    {
-      id: 2,
-      title: 'Jackets',
-    },
-    {
-      id: 3,
-      title: 'Sneakers',
-    },
-    {
-      id: 4,
-      title: 'Women',
-    },
-    {
-      id: 5,
-      title: 'Men',
-    },
-  ];
-
-  const renderCategory = categories.map((category) => {
-    return (
-      <div key={category.id} className="category-container">
-        <div className="background-image"></div>
-        <div className="category-body-container">
-          <h2>{category.title}</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-    );
-  });
-
-  return <div className="categories-container">{renderCategory}</div>;
+  return <Directory />;
 };
 
 export default App;
