1bc16b8ea9b50dcdfe8ca82b578e456b6f49f91e
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-28 14:33:21 +0900

    crwn-clothing/Section6-94

--- crwn-clothing/src/routes/sign-in/sign-in.js
+++ crwn-clothing/src/routes/sign-in/sign-in.js
@@ -1,9 +1,12 @@
-import { signInWithGooglePopup } from '../../utils/firebase/firebase.utils';
+import {
+  createUserDocumentFromAuth,
+  signInWithGooglePopup,
+} from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
   const logGoogleUser = async () => {
     const res = await signInWithGooglePopup();
-    console.log(res);
+    const userDocRef = await createUserDocumentFromAuth(res.user);
   };
 
   return (
