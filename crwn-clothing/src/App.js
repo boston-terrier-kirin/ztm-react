f807c8bb73fd2d5221b626ff2459161ef252352f
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-29 08:04:11 +0900

    crwn-clothing/Section6-102

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -2,14 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 
 import Home from './routes/home/home';
 import Navigation from './routes/navigation/navigation';
-import SignIn from './routes/sign-in/sign-in';
+import Auth from './routes/auth/auth';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />}></Route>
-        <Route path="sign-in" element={<SignIn />}></Route>
+        <Route path="auth" element={<Auth />}></Route>
       </Route>
     </Routes>
   );
---
+++ crwn-clothing/src/components/sign-in-form/sign-in-form.js
@@ -0,0 +1,86 @@
+import { useState } from 'react';
+import {
+  createUserDocumentFromAuth,
+  signInAuthUserWithEmailAndPassword,
+  signInWithGooglePopup,
+} from '../../utils/firebase/firebase.utils';
+import Button from '../button/botton';
+import FormInput from '../form-input/form-input';
+import './sign-in-form.scss';
+
+const defaultFormFilelds = {
+  email: '',
+  password: '',
+};
+
+const SignInForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFilelds);
+  const { email, password } = formFields;
+
+  const restFormFields = () => {
+    setFormFields(defaultFormFilelds);
+  };
+
+  const signInWithGoogle = async () => {
+    const res = await signInWithGooglePopup();
+    await createUserDocumentFromAuth(res.user);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const res = await signInAuthUserWithEmailAndPassword(email, password);
+      restFormFields();
+    } catch (err) {
+      if (
+        err.code === 'auth/user-not-found' ||
+        err.code === 'auth/wrong-password'
+      ) {
+        alert('Invalid credentials.');
+      }
+      console.log(err);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  return (
+    <div className="sign-in-container">
+      <h2>Already have an account?</h2>
+      <span>Sign in with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label="Email"
+          name="email"
+          type="email"
+          required
+          value={email}
+          onChange={handleChange}
+        />
+
+        <FormInput
+          label="Password"
+          name="password"
+          type="password"
+          required
+          value={password}
+          onChange={handleChange}
+        />
+
+        <div className="buttons-container">
+          <Button>Sign In</Button>
+          <Button type="button" onClick={signInWithGoogle} buttonType="google">
+            Google sign in
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignInForm;
