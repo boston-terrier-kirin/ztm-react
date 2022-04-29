39084440d32a3261afe8d574919fddcb6318e108
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-30 05:31:37 +0900

    crwn-clothing/Section11-140 styled-components はスキップ

--- crwn-clothing/src/components/directory-item/directory-item.js
+++ crwn-clothing/src/components/directory-item/directory-item.js
@@ -1,10 +1,16 @@
+import { useNavigate } from 'react-router-dom';
 import './directory-item.styles.scss';
 
 const DirectoryItem = (props) => {
   const { category } = props;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => {
+    navigate(category.route);
+  };
 
   return (
-    <div className="directory-item-container">
+    <div onClick={onNavigateHandler} className="directory-item-container">
       <div
         className="background-image"
         style={{ backgroundImage: `url(${category.imageUrl})` }}
