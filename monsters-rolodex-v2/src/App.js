0da9f44ae56c486bf84ad7a536cd7115ae43374b
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-05-06 07:15:18 +0900

    monsters-rolodex-v2/Section3-59

--- monsters-rolodex-v2/src/App.js
+++ monsters-rolodex-v2/src/App.js
@@ -1,78 +1,54 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import CardList from './components/card-list/card-list';
 import Dummy from './components/card-list/dummy';
 import SearchBox from './components/search-box/search-box';
 
 import './App.css';
 
-class App extends React.Component {
-  constructor() {
-    console.log('App.constructor');
-    super();
+const App = () => {
+  console.log('App');
 
-    this.state = {
-      searchTerm: '',
-      monsters: [],
-    };
-  }
+  const [searchTerm, setSearchTerm] = useState('');
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
-  componentDidMount() {
-    console.log('App.componentDidMount');
+  useEffect(() => {
+    console.log('App.useEffect_1');
 
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((res) => res.json())
-      .then((users) => {
-        this.setState(
-          () => {
-            console.log('App.componentDidMount.setState1');
-            return { monsters: users, filteredMonsters: users };
-          },
-          () => {
-            console.log('App.componentDidMount.setState2');
-          }
-        );
-      });
-  }
+      .then((users) => setMonsters(users));
+  }, []);
 
-  /**
-   * onChangeに直接書くのではなく、外出しにしておけば、関数が1回しか作られないので、性能は良くなるらしい。
-   */
-  onSearchChange = (event) => {
-    const searchTerm = event.target.value.toLocaleLowerCase();
-    this.setState(
-      () => {
-        console.log('App.onSearchChange_1');
-        return { searchTerm };
-      },
-      () => {
-        console.log('App.onSearchChange_2');
-      }
-    );
-  };
+  // 検索条件が変わった場合のみfilterを走らせたいので、useEffectを使う。
+  useEffect(() => {
+    console.log('App.useEffect_2');
 
-  render() {
-    console.log('App.render');
-
-    const { searchTerm, monsters } = this.state;
-    const { onSearchChange } = this;
-
-    const filteredMonsters = monsters.filter((monster) =>
+    const filtered = monsters.filter((monster) =>
       monster.name.toLowerCase().includes(searchTerm)
     );
+    setFilteredMonsters(filtered);
+  }, [searchTerm, monsters]);
 
-    return (
-      <div>
-        <h1 className="app-title">Monsters Rolodex</h1>
-        <SearchBox
-          onChangeHandler={onSearchChange}
-          placeholder="search monsters"
-          className="monsters-search-box"
-        />
-        <Dummy />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
-}
+  const onSearchChange = (event) => {
+    console.log('App.onSearchChange');
+
+    const value = event.target.value.toLocaleLowerCase();
+    setSearchTerm(value);
+  };
+
+  return (
+    <div>
+      <h1 className="app-title">Monsters Rolodex</h1>
+      <SearchBox
+        onChangeHandler={onSearchChange}
+        placeholder="search monsters"
+        className="monsters-search-box"
+      />
+      <Dummy />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
 
 export default App;
