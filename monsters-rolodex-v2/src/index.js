7a512a5eae11a1c91c3e2a536bb11866b141c328
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-05-06 07:55:19 +0900

    monsters-rolodex-v2/Section3-62

--- monsters-rolodex-v2/src/index.js
+++ monsters-rolodex-v2/src/index.js
@@ -4,8 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// v18からReact.StrictModeを使っている場合、コンソールのログが2重で出てしまう。
+// React Dev Tools をインストールしていれば、グレーで表示される。
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
